c925c681351636bde044def14361dd207b6623c4
Talita Beneves <talitabeneves15@gmail.com>
2021-12-26 20:24:25 -0300

    Organizando projeto, definindo rotas, criando alerts

---
+++ Front/Eventos-App/src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from "ngx-spinner";
+
+import { Evento } from '../../models/Evento';
+import { EventoService } from '../../services/evento.service';
+
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public MostraImg: boolean = true;
+  public modalRef?: BsModalRef;
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados:  Evento[] = [];
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter (
+      (evento: any ) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1  ||
+                        evento.local.toLocaleLowerCase().indexOf(filtrarPor)!== -1  ||
+                        evento.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
+
+  public ngOnInit(): void {
+    this.getEventos();
+    this.spinner.show();
+
+    setTimeout(() => {
+    }, 3000);
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error:(error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao Carregar os Eventos', 'Erro!')
+      },
+      complete: () => this.spinner.hide()
+    });
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+
+
+}
+
+
