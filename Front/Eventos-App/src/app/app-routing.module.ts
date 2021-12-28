98def7afafffb6a20f1873dcc3ad1dbb027f0be1
Talita Beneves <talitabeneves15@gmail.com>
2021-12-27 22:16:17 -0300

    Atualizando projeto e Definindo Layout

--- Front/Eventos-App/src/app/app-routing.module.ts
+++ Front/Eventos-App/src/app/app-routing.module.ts
@@ -1,17 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ContatosComponent } from './components/contatos/contatos.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+
+import { UserComponent } from './components/user/user.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent },
+    ]
+   },
+   {
+     path: 'eventos', component: EventosComponent,
+     children: [
+       { path: 'detalhe/:id', component: EventoDetalheComponent },
+       { path: 'detalhe', component: EventoDetalheComponent },
+       { path: 'lista', component: EventoListaComponent }
+      ]
+    },
+  { path: 'eventos', redirectTo: 'eventos/lista' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'eventos', component: EventosComponent },
   { path: 'contatos', component: ContatosComponent },
-  { path: 'perfil', component: PerfilComponent },
+  { path: 'user/perfil', component: PerfilComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
