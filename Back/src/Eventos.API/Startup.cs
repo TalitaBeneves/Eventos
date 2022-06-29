180e3e939c7107496a82237ee4c06e7002c35b70
Talita Beneves <talitabeneves15@gmail.com>
2022-06-29 02:30:31 -0300

    Projeto rodando

--- Back/src/Eventos.API/Startup.cs
+++ Back/src/Eventos.API/Startup.cs
@@ -16,65 +16,65 @@ using ProEventos.Application;
 
 namespace Eventos.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddDbContext<EventosContext>(
-                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
-            );
-            services.AddControllers()
-                    .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = 
-                        Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                    );
-
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddScoped<IEventoService, EventoService>();
-            services.AddScoped<IGeralPersist, GeralPersist>();
-            services.AddScoped<IEventoPersist, EventoPersist>();
-
-            services.AddCors();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Eventos.API", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eventos.API v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseCors(acess => acess.AllowAnyHeader()
-                                      .AllowAnyMethod()
-                                      .AllowAnyOrigin()
-            );
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddDbContext<EventosContext>(
+          context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+      );
+      services.AddControllers()
+              .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling =
+                  Newtonsoft.Json.ReferenceLoopHandling.Ignore
+              );
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+      services.AddScoped<IEventoService, EventoService>();
+      services.AddScoped<IGeralPersist, GeralPersist>();
+      services.AddScoped<IEventoPersist, EventoPersist>();
+
+      services.AddCors();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Eventos.API", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eventos.API v1"));
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseCors(acess => acess.AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowAnyOrigin()
+      );
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
